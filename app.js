160c84708339576046bb21f9111ed8ea1a7ae1d4
Shubh <rathodshubham7711@gmail.com>
2026-06-11 21:48:47 +0530

    ui changed

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const dotenv = require("dotenv").config();
 // dotenv.config({ path: "./config.env" });
+app.use(express.static(path.resolve("public")));
 
-app.get("/", (req, res) => {});
+app.set("view engine", "ejs");
+app.set("views", path.resolve("views"));
+
+app.get("/", (req, res) => {
+  res.render("Landing/dashboard");
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const dotenv = require("dotenv").config();
 // dotenv.config({ path: "./config.env" });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`server started on port ${process.env.PORT}`);
+  console.log(`server started on port on http://localhost:${process.env.PORT}`);
 });
